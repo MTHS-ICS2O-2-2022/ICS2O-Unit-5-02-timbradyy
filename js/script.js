7892e1f62c6ab3eb1c275e8dd1e24b0050d1dcd8
timbradyy <124389347+timbradyy@users.noreply.github.com>
2023-05-15 16:47:45 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Tim Brady
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+
+const randomNumberPos = Math.floor(Math.random() * 6) + 1
+const randomNumberNeg = Math.floor(Math.random() * -6) + 1
+
+/**
+ * This function displays positive and negative number
+ */
+function myButtonClicked() {
+  buttonOnChecked = document.getElementById("positive-checked").checked
+
+  if (buttonOnChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+      "Your random number is: " + randomNumberPos + "."
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+      "Your random number is: " + randomNumberNeg + "."
+  }
+}
